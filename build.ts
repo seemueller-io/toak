30582d3f34a7594185a5b8fb586a0b69d0006d17
geoffsee <>
2025-04-06 11:07:47 -0400

    remove extra build logic

--- build.ts
+++ build.ts
@@ -2,9 +2,17 @@ import isolatedDecl from 'bun-plugin-isolated-decl';
 
 // handles building the library
 await Bun.build({
-  entrypoints: ['./src/*.ts'],
+  entrypoints: [
+    "src/cli.ts",
+    "src/fileExclusions.ts",
+    "src/fileTypeExclusions.ts",
+    "src/index.ts",
+    "src/MarkdownGenerator.ts",
+    "src/TokenCleaner.ts"
+  ],
   outdir: './dist',
   target: 'node',
+  splitting: true,
   plugins: [
     isolatedDecl({
       forceGenerate: true,  // Generate declaration files even if there are errors
